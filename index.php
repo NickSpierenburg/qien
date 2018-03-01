b72fbe82814c98b46fa519fd22dd78a87c9efa45
Nick <nickspierenburg@hotmail.com>
2018-03-01 15:17:10 +0100

    Calculator

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-HOI
+<?php
+
+$g1 = $_GET['g1'];
+$g2 = $_GET['g2'];
+$func = $_GET['func'];
+
+switch ($func) {
+	case 'plus':
+		plus($g1, $g2);
+		break;
+	case 'min':
+		minus($g1, $g2);
+		break;
+	case 'keer':
+		keer($g1, $g2);
+		break;
+	case 'deel':
+		deel($g1, $g2);
+		break;
+	default:
+		echo 'Er is geen func meegegeven!';
+		break;
+}
+
+function plus($g1, $g2) {
+	$ans = $g1 + $g2;
+	echo $g1 . ' + ' . $g2 . ' = ' . $ans;
+}
+
+function minus($g1, $g2) {
+	$ans = $g1 - $g2;
+	echo $g1 . ' - ' . $g2 . ' = ' . $ans;
+}
+
+function keer($g1, $g2) {
+	$ans = $g1 * $g2;
+	echo $g1 . ' * ' . $g2 . ' = ' . $ans;
+}
+
+function deel($g1, $g2) {
+	$ans = $g1 / $g2;
+	echo $g1 . ' / ' . $g2 . ' = ' . $ans;
+}
+
+?>
